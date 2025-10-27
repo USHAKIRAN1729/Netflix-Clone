abeb3e93dca048e7cc345e6c2679a2745cb3a0e4
BODDEPALLI USHAKIRAN <131387978+USHAKIRAN1729@users.noreply.github.com>
2025-10-27 18:15:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ let accordian = document.getElementsByClassName("FAQ__title");
 
 for (let i = 0; i < accordian.length; i++) {
   accordian[i].addEventListener("click", function () {
-    if (this.childNodes[1].classList.contains("fa-plus")) {
-      this.childNodes[1].classList.remove("fa-plus");
-      this.childNodes[1].classList.add("fa-times");
+
+    let icon = this.querySelector(".faq-icon");
+
+    if (icon.classList.contains("fa-plus")) {
+      icon.classList.remove("fa-plus");
+      icon.classList.add("fa-times");
     } else {
-      this.childNodes[1].classList.remove("fa-times");
-      this.childNodes[1].classList.add("fa-plus");
+      icon.classList.remove("fa-times");
+      icon.classList.add("fa-plus");
     }
 
     let content = this.nextElementSibling;
